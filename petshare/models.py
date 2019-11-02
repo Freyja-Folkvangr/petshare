c7d0b656ae4fc628ef98779a8d7580c3ecae2f8e
Giuliano P. Tognarelli Buono-core <gtognarelli@linux.com>
2019-11-02 13:03:37 -0300

    added pet types, pet names, comment

--- petshare/models.py
+++ petshare/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    TYPES = (
+        (0, 'Other'),
+        (1, 'Cat'),
+        (2, 'Dog'),
+        (3, 'Guinea Pig'),
+        (4, 'Hamster'),
+        (5, 'Poring'),
+        (6, 'Dragon')
+    )
+    nickname = models.CharField(max_length=128, null=False, default='Unknown')
+    pet_name = models.CharField(max_length=128, null=False, default='Unknown Pet')
+    pet_type = models.IntegerField(choices=TYPES, null=False, default=0)
+    comment = models.CharField(max_length=128, null=True, default='')
+    photo = models.FileField(upload_to='uploads', verbose_name='filepath', default=None)
+    date_posted = models.DateTimeField(verbose_name='Upload date', auto_now=True)
+
+class Vote(models.Model):
+    nickname = models.CharField(max_length=128, null=False, default='Unknown')
+    photo_id = models.ForeignKey(Post, on_delete=models.CASCADE)
