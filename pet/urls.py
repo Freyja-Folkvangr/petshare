3de2629084dcc70083f1c024c40b1dd006ecd83b
Giuliano P. Tognarelli Buono-core <gtognarelli@linux.com>
2019-11-02 12:26:37 -0300

    added index page

--- pet/urls.py
+++ pet/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from petshare.views import index
 
 urlpatterns = [
+    url(r'^$', index, name='home'),
+    url(r'^$', index, name='index'),
     url(r'^admin/', admin.site.urls),
 ]
